fd3b1f22f88c2482f635e98c55c4a1e6e0f78d91
Michael Hinrichs <michaelghinrichs@gmail.com>
2015-07-13 07:59:11 -0700

    scraping but no selecting

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/scrape', function(req, res){
+
+    url = 'http://www.cnn.com';
+
+    // The structure of our request call
+    // The first parameter is our URL
+    // The callback function takes 3 parameters, an error, response status code and the html
+
+    request(url, function(error, response, html){
+
+        // First we'll check to make sure no errors occurred when making the request
+
+        if(!error){
+            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+            var $ = cheerio.load(html);
+
+            // Finally, we'll define the variables we're going to capture
+
+            var title, link;
+            var json = { title : "", link : ""};
+        }
+    })
+})
+
+app.listen('8081')
+console.log('Magic happens on port 8081');
+exports = module.exports = app;
